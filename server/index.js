86a9444c5903f9bd3546e72da66665d186ced147
stasrusnak <stasrusnak@gmail.com>
2023-01-07 09:27:36 +0200

    update host

--- server/index.js
+++ server/index.js
@@ -16,13 +16,13 @@ app.use("/api/posts", posts);
 
 
 //Handle production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     // Static folder
     app.use(express.static(__dirname + '/public'));
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
+// }
 
 const port = process.env.PORT || 5000;
 
